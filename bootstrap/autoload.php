46faa06b62ec087226c57f2f910bb6e57f09880f
penn <penn@pbit888.com>
2018-07-06 18:39:51 +0800

    2018-7-6更新

--- bootstrap/autoload.php
+++ bootstrap/autoload.php
@@ -1,11 +1,26 @@
 <?php
 use Symfony\Component\HttpFoundation\Request;
-
+use Symfony\Component\DependencyInjection\Reference;
 $routes = include __DIR__.'/app.php';
 $container = include __DIR__.'/container.php';
 
-$request = Request::createFromGlobals();
+// 设置debug
+$container->setParameter('debug', true);
 
+// 设置详情，编码
+$container->register('listener.response', HttpKernel\EventListener\ResponseListener::class)
+    ->setArguments(array('%charset%'))
+;
+$container->setParameter('charset', 'UTF-8');
+
+// 控制路由中间件
+$container->register('matcher', Routing\Matcher\UrlMatcher::class)
+    ->setArguments(array('%routes%', new Reference('context')))
+;
+$container->setParameter('routes', include __DIR__.'/app.php');
+
+
+$request = Request::createFromGlobals();
 $response = $container->get('framework')->handle($request);
 
 $response->send();
