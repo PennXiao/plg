04d72ea6dc44d6445054752706a2826cdc052f36
penn <pennilessfor@gmail.com>
2018-07-10 22:29:09 +0800

    增加yml文件路由定义

--- bootstrap/routes.php
+++ bootstrap/routes.php
@@ -1,14 +1,22 @@
 <?php
-use Symfony\Component\Routing\RouteCollection;
-use Symfony\Component\Routing\Route;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Routing\Loader\YamlFileLoader;
+ 
+// look inside *this* directory 锁定“这个”目录来查找
+$locator = new FileLocator(array(BASEDIR_.'/config'));
+$loader = new YamlFileLoader($locator);
+$routes = $loader->load('routes.yml');
 
-$routes = new RouteCollection();
-$routes->add('master', new Route('/',
-    array(
-        '_controller' => '\App\Controller\HomeController::index',
-    )
-));
+// use Symfony\Component\Routing\RouteCollection;
+// use Symfony\Component\Routing\Route;
 
-// ...
+// $routes = new RouteCollection();
+// $routes->add('master', new Route('/',
+//     array(
+//         '_controller' => '\App\Controller\HomeController::index',
+//     )
+// ));
+
+// // ...
 
 return $routes;
