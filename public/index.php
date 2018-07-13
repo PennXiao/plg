6609acb06e7a0c1b3219d6becd534be2d453fa01
penn <penn@pbit888.com>
2018-07-13 14:05:06 +0800

    美化入口文件代码

--- public/index.php
+++ public/index.php
@@ -1,6 +1,20 @@
 <?php
-define('BASEDIR_', realpath(__DIR__.'\..\/'));
+/**
+ * @author pennilessfor@gmail.com
+ * @version 1.0
+ * @link 
+ */
 
+//定义项目根目录
+define('BASEDIR_', realpath(__DIR__.'\..\/'));
+/**
+ * 引入composer autoload
+ * 自动加载拓展与类库
+ */
 require_once BASEDIR_.'/vendor/autoload.php';
+/**
+ * 运行框架
+ * @var [obj] $app
+ */
 $app = require BASEDIR_.'/bootstrap/autoload.php';
 $app->send();
